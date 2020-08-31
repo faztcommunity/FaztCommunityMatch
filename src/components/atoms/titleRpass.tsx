95bda637cb56842ad700ca1ff641e52f36c7fa1d
Eslabi <deividelpaisa@hotmail.com>
2020-08-31 00:32:33 -0500

    atom title form password added

---
+++ src/components/atoms/titleRpass.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TitleRpass: React.FC = () => {
+  return <h2 className="text-registerpass">¿Que esperas para registrarte?</h2>
+}
+
+export default TitleRpass
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import InputPass from '../../components/molecules/input-pass'
 import FormBackground from '../../components/organisms/form/Oform'
 import ConfirmPass from '../../components/molecules/inputs/input-confirmpass'
+import TitleRpass from '../../components/atoms/titleRpass'
 
 const Register: React.FC = () => {
   return (
     <>
       <form action="" className="content-form">
+        <TitleRpass />
         <div className="content-input">
           <InputPass />
           <ConfirmPass />
