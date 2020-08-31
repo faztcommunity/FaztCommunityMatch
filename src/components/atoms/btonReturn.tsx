926edc84d940c7c6d7831ebec28e255391ec4907
Eslabi <deividelpaisa@hotmail.com>
2020-08-31 11:30:02 -0500

    atom button return created

---
+++ src/components/atoms/btonReturn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BtnmReturn: React.FC = () => {
+  return (
+    <a className="btn-medium-return" href="#">
+      Volver
+    </a>
+  )
+}
+
+export default BtnmReturn
