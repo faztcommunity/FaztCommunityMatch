63e89a9c2a8008d3bf027fa4958966d91cc66ae3
Eslabi <deividelpaisa@hotmail.com>
2020-08-31 15:25:39 -0500

    molecule description log-in finished

---
+++ src/components/molecules/description-login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DescriptionLogin: React.FC = () => {
+  return (
+    <h3 className="have-account">
+      ¿Ya tienes una cuenta?
+      <a href="#">
+        <span className="log-in"> Inicia sesión</span>
+      </a>
+    </h3>
+  )
+}
+
+export default DescriptionLogin
--- src/components/organisms/form/pagetwo.tsx
+++ src/components/organisms/form/pagetwo.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import BtnNext from '../../atoms/btonNext'
 import FormBackground from '../background/backgroundform'
+import DescriptionLogin from '../../molecules/description-login'
 
 const PageFormTwo: React.FC = () => {
   return (
     <div className="content-form">
+      <p className="content-description">
+        <DescriptionLogin />
+      </p>
       <div className="container-btn-next">
         <BtnNext />
       </div>
