c7fd92ee131e8e587a31eacbf96a6d485f381a05
Eslabi <deividelpaisa@hotmail.com>
2020-08-31 12:42:29 -0500

    correction organization

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,31 +1,13 @@
 import React from 'react'
 
-import InputPass from '../../components/molecules/input-pass'
-import FormBackground from '../../components/organisms/form/Oform'
-import ConfirmPass from '../../components/molecules/inputs/input-confirmpass'
-import TitleRpass from '../../components/atoms/titleRpass'
-import BtnMblue from '../../components/atoms/btnMblue'
-import BtnmReturn from '../../components/atoms/btonReturn'
+import PageFormOne from '../../components/organisms/form/pageone'
 
 const Register: React.FC = () => {
   return (
     <>
-      <form action="" className="content-form">
-        <TitleRpass />
-        <div className="content-input">
-          <InputPass />
-          <ConfirmPass />
-        </div>
-        <div className="container-btns-register">
-          <div className="containerbtn-return">
-            <BtnmReturn />
-          </div>
-          <div className="containerbtn-blue">
-            <BtnMblue />
-          </div>
-        </div>
-        <FormBackground />
-      </form>
+      <div>
+        <PageFormOne />
+      </div>
     </>
   )
 }
