207a0d157b0c5f709bb399fb2e25d141d137f80f
Eslabi <deividelpaisa@hotmail.com>
2020-08-31 20:18:02 -0500

    change of styles

---
+++ src/components/atoms/titlebasicdates.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TitleBasicDates: React.FC = () => {
+  return <h2 className="title-basic-dates">¿Que esperas para registrarte?</h2>
+}
+
+export default TitleBasicDates
