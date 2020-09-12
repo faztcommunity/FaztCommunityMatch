1217497ebe1f80b555a458c8a5818823d9ed6186
Eslabi <deividelpaisa@hotmail.com>
2020-09-11 21:33:56 -0500

    atom made created

--- src/components/atoms/BtonAcceptTerms.tsx
+++ src/components/atoms/BtonAcceptTerms.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useRef } from 'react'
+import ModalRegisterDone from '../organisms/form/ModalRegisterDone'
 
 function BtonAcceptTerms() {
+  const modalDoneRef = useRef<any>(null)
+
+  const openModal = () => {
+    modalDoneRef.current.openModal()
+  }
+
   return (
     <>
-      <a className="accept-terms">Si, Continuar</a>
+      <a onClick={openModal} className="accept-terms">
+        Si, Continuar
+      </a>
+      <ModalRegisterDone ref={modalDoneRef}></ModalRegisterDone>
     </>
   )
 }
