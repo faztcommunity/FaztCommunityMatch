8f0dc52a0c0704ebca21511600a790383ba5448a
Eslabi <deividelpaisa@hotmail.com>
2020-09-14 20:26:03 -0500

    atom free card title created

---
+++ src/components/atoms/plans/TitleCardFree.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TitleCardFree() {
+  return (
+    <div className="content-title-free">
+      <h2 className="title-card-free">
+        Plan <span className="tittle-free">Gratuito</span>
+      </h2>
+    </div>
+  )
+}
+
+export default TitleCardFree
--- src/components/organisms/plans/Plans.tsx
+++ src/components/organisms/plans/Plans.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import BtonFree from '../../atoms/plans/BtonFree'
+import TitleCardFree from '../../atoms/plans/TitleCardFree'
 import CardFree from '../../molecules/plans/CardFree'
 import CardFreeDescription from '../../molecules/plans/CardFreeDescription'
 
 function PlansCards() {
   return (
     <div className="content-card-free">
+      <TitleCardFree />
       <CardFreeDescription />
       <CardFree />
       <BtonFree />
