8fee311ec24c1ea2c51ce000200f2f3780612e89
Eslabi <deividelpaisa@hotmail.com>
2020-09-17 23:19:48 -0500

    molecule buttons nav created and hover created

--- src/components/molecules/profile/BtonsMenu.tsx
+++ src/components/molecules/profile/BtonsMenu.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
+import IconProject from '../../atoms/profile/IconProject'
+import IconTest from '../../atoms/profile/IconTest'
 
 function BtonsMenu() {
   return (
     <div className="content-btns-menu">
       <Link href="/tests">
-        <a>Realizar Test</a>
+        <div className="content-test">
+          <div className="background-hover">
+            <IconTest />
+            <a className="test">Realizar Test</a>
+          </div>
+        </div>
       </Link>
-      <Link href="/proyecto">
-        <a>Proyecto</a>
-      </Link>
+      <div className="content-project">
+        <Link href="/proyecto">
+          <div className="background-hover">
+            <IconProject />
+            <a className="project">Proyecto</a>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
--- src/components/organisms/profile/Profile.tsx
+++ src/components/organisms/profile/Profile.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import BtonsMenu from '../../molecules/profile/BtonsMenu'
 import PrincipalNav from '../../molecules/profile/PrincipalNav'
 
 function Profile() {
   return (
     <>
       <nav>
+        <BtonsMenu />
         <PrincipalNav />
       </nav>
     </>
