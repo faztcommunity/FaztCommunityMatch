a87f13cc98a6039e8e733ed7c0f565e5cc2583aa
Eslabi <deividelpaisa@hotmail.com>
2020-09-25 17:41:38 -0500

    molecule user description added

---
+++ src/components/molecules/profile/DescriptionUser.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DescriptionUser() {
+  return (
+    <div className="content-description-user">
+      <h3 className="text-description-user">Descripción</h3>
+      <input type="text" className="box-description-user" />
+    </div>
+  )
+}
+
+export default DescriptionUser
--- src/components/organisms/profile/SectionUserInfo.tsx
+++ src/components/organisms/profile/SectionUserInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import TitleNameUser from '../../atoms/profile/TitleNameUser'
 import BackgroundProfileUser from '../../molecules/profile/BackgroundProfileUser'
 import CircleUser from '../../molecules/profile/CircleUser'
+import DescriptionUser from '../../molecules/profile/DescriptionUser'
 import MenuCardUser from '../../molecules/profile/MenuCardUser'
 
 function SectionUserInfo() {
@@ -11,6 +12,7 @@ function SectionUserInfo() {
       <CircleUser />
       <BackgroundProfileUser />
       <MenuCardUser />
+      <DescriptionUser />
     </div>
   )
 }
