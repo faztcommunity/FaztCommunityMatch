90e4b6fa07ff35f42c352537da75999ead5518aa
Eslabi <deividelpaisa@hotmail.com>
2020-09-28 21:44:12 -0500

    atom background abilities section added

---
+++ src/components/atoms/profile/BackgroundSectionAbilities.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BtonCreateTest from '../../atoms/profile/BtonCreateTest'
+import TitleCreateTest from '../../atoms/profile/TitleCreateTest'
+
+function BackgroundSectionAbilities() {
+  return (
+    <>
+      <div className="section-abilities"></div>
+    </>
+  )
+}
+
+export default BackgroundSectionAbilities
--- src/components/molecules/profile/SectionAbilitiesUser.tsx
+++ src/components/molecules/profile/SectionAbilitiesUser.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import BackgroundSectionAbilities from '../../atoms/profile/BackgroundSectionAbilities'
 
 function SectionAbilitiesUser() {
   return (
     <>
       <span className="line-hover-abilities-on"></span>
-      <h2>Estas son tus habilidades</h2>
+      <BackgroundSectionAbilities />
     </>
   )
 }
