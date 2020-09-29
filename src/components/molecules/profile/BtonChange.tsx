7cee3f74002cfc4cfe8dee1e8fdf4a9f52f7d532
Eslabi <deividelpaisa@hotmail.com>
2020-09-29 15:19:15 -0500

    atom icon info change added

--- src/components/molecules/profile/BtonChange.tsx
+++ src/components/molecules/profile/BtonChange.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import IconChange from '../../atoms/profile/IconChange'
+import IconInfoChange from '../../atoms/profile/IconInfoChange'
 
 function BtonChange() {
   return (
     <>
       <div className="content-hover-circle">
+        <IconInfoChange />
         <div className="hover-circle"></div>
 
         <IconChange />
