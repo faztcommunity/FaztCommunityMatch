7a394ddb75a7aef97c550931600f69d951c599c6
Eslabi <deividelpaisa@hotmail.com>
2020-09-29 14:26:00 -0500

    molecule icon change added

---
+++ src/components/molecules/profile/BtonChange.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import IconChange from '../../atoms/profile/IconChange'
+
+function BtonChange() {
+  return (
+    <>
+      <div className="content-hover-circle">
+        <div className="hover-circle"></div>
+
+        <IconChange />
+      </div>
+    </>
+  )
+}
+
+export default BtonChange
--- src/pages/perfil/index.tsx
+++ src/pages/perfil/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import NavProfile from '../../components/organisms/profile/NavProfile'
 import SectionUserInfo from '../../components/organisms/profile/SectionUserInfo'
 import RecordUser from '../../components/molecules/profile/RecordUser'
+import BtonChange from '../../components/molecules/profile/BtonChange'
 
 function Plans() {
   return (
@@ -20,6 +21,7 @@ function Plans() {
       <section>
         <RecordUser />
       </section>
+      <BtonChange />
     </>
   )
 }
