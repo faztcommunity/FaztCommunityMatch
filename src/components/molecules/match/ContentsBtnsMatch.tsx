4baf82e27cbfcf3b8351ad9fd8b352f47943f801
Eslabi <deividelpaisa@hotmail.com>
2020-10-17 13:40:10 -0500

    Atoms buttons match created

--- src/components/molecules/match/ContentsBtnsMatch.tsx
+++ src/components/molecules/match/ContentsBtnsMatch.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import IconBtnMatch from '../../atoms/match/IconBtnMatch'
+import IconDontLike from '../../atoms/match/IconDontLike'
+import IconGiveHeart from '../../atoms/match/IconGiveHeart'
+import IconGiveStar from '../../atoms/match/IconGiveStar'
+import IconNext from '../../atoms/match/IconNext'
+import IconPrevius from '../../atoms/match/IconPrevius'
 
 function ContentsBtnsMatch() {
   return (
-    <div className="content-btns-match">
+    <div className="content-card-match">
+        <div className="content-btns-match">
+        <IconPrevius />
+        <IconDontLike />
+        <IconGiveStar />
+        <IconGiveHeart />
+        <IconBtnMatch />
+        <IconNext />
+        </div>
+        <div className="background-btns-match"></div>
     </div>
   )
 }
