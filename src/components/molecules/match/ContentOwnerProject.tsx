354b09bcae0fedc91435566212c119a67c20cbbb
Eslabi <deividelpaisa@hotmail.com>
2020-10-16 21:15:26 -0500

    molecule propietary section created

---
+++ src/components/molecules/match/ContentOwnerProject.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function ContentOwnerProject() {
+  return (
+    <article className="content-owner-project">
+      <div className="card-owner-project">
+     <h4 className="style-title-owner"> Owner</h4>
+     <h3 className="name-owner">Nombre dueño del proyecto. <span className="line-title-name-owner"></span></h3>
+<p className="owner-description"></p>
+      </div>
+    </article>
+  )
+}
+
+export default ContentOwnerProject
--- src/components/organisms/match/CardMatchProject.tsx
+++ src/components/organisms/match/CardMatchProject.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import ContentDescriptionProject from '../../molecules/match/ContentDescriptionProject'
 import ContentImageProjec from '../../molecules/match/ContentImageProject'
 import ContentNameProject from '../../molecules/match/ContentNameProject'
+import ContentOwnerProject from '../../molecules/match/ContentOwnerProject'
 import ContentTypeProject from '../../molecules/match/ContentTypeProject'
 
 function CardMatchProject() {
   return (
     <section className="card-match-project">
       <ContentImageProjec />
-      <div>
+      <div className="section-card-project-match">
       <ContentNameProject />
       <ContentDescriptionProject />
       <ContentTypeProject />
       </div>
+      <ContentOwnerProject />
     </section>
   )
 }
