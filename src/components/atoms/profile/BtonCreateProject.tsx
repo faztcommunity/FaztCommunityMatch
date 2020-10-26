2c87860039ed56b89c724862cedaea2f46169fbe
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-26 15:52:20 -0500

    Path button create project added

--- src/components/atoms/profile/BtonCreateProject.tsx
+++ src/components/atoms/profile/BtonCreateProject.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function BtonCreateProject() {
   return (
     <>
+    <Link href="/crear-proyecto">
       <button className="btn-create-project">Crear Proyecto</button>
+      </Link>
     </>
   )
 }
