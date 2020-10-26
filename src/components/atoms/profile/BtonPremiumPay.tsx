d750cb8674e547a1ca82e8c0504105b53f132cfd
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-25 22:41:21 -0500

    Route added to premium button

--- src/components/atoms/profile/BtonPremiumPay.tsx
+++ src/components/atoms/profile/BtonPremiumPay.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function BtonPremiumPay() {
   return (
     <>
+    <Link href="/planes">
       <button className="btn-premium-pay">Vuelvete Premium</button>
+    </Link>
     </>
   )
 }
