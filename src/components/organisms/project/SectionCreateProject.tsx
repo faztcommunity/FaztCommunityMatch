2c5686d9ddab3bd3a292e13d68434aa7a6479fc8
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-26 14:08:45 -0500

    Create molecule title and description section create project

--- src/components/organisms/project/SectionCreateProject.tsx
+++ src/components/organisms/project/SectionCreateProject.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ImagesCreateProject from '../../molecules/project/ImagesCreateProject'
+import TitleDescriptionCreateProject from '../../molecules/project/TitleDescriptionCreateProject'
 
 
 function SectionCreateProject() {
   return (
-    <section>
+    <section className="section-create-project">
     <ImagesCreateProject />
-
+    <TitleDescriptionCreateProject />
     </section>
   )
 }
