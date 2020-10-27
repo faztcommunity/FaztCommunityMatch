bdfa42b49f367950ca6fd877997ea3e2f2934da9
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-26 22:24:27 -0500

    Create information container molecule images on the create project page

--- src/components/molecules/create-project/ContentInfo.tsx
+++ src/components/molecules/create-project/ContentInfo.tsx
@@ -5,7 +5,8 @@ import HelpButton from '../../atoms/create-project/HelpButton'
 function ContentInfo() {
   return (
     <>
-        <div>
+        <div className="content-black-info">
+          <h2 className="txt-info-create-project">Imagenes del Proyecto</h2>
         <HelpButton />
         </div>
     </>
