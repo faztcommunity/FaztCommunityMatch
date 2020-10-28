83c52856ed4c2e9b9e20e2a1b1bc13230ce1678c
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-28 16:32:43 -0500

    Organism add project requirements added

--- src/components/organisms/create-project/ColumnProjectThree.tsx
+++ src/components/organisms/create-project/ColumnProjectThree.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AddRequirementsProject from '../../molecules/create-project/AddRequirementsProject'
 import ContentProjectRequirements from '../../molecules/create-project/ContentProjectRequirements'
 
 function ColumnProjectThree() {
   return (
 <article className="article-column-project-three">
   <ContentProjectRequirements />
+  <AddRequirementsProject />
 </article>
   )
 }
