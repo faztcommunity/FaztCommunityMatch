c5363fec46a9498c506428b399b081c46f371189
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-27 23:24:32 -0500

    Create molecule input project description

---
+++ src/components/molecules/create-project/InputDescriptionProject.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function InputDescriptionProject() {
+  return (
+    <>
+        <div className="content-input-description-project">
+        <textarea className="input-description-project" placeholder="Este proyecto..."></textarea>
+<span className="count-characters">0/500</span>
+        </div>
+    </>
+  )
+}
+
+export default InputDescriptionProject
--- src/components/organisms/create-project/ColumnProjectTwo.tsx
+++ src/components/organisms/create-project/ColumnProjectTwo.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ContentDescriptionProject from '../../molecules/create-project/ContentDescriptionProject'
 import ContentTitleProject from '../../molecules/create-project/ContentTitleProject'
+import InputDescriptionProject from '../../molecules/create-project/InputDescriptionProject'
 import InputEditTitleProject from '../../molecules/create-project/InputEditTitleProject'
 
 function ColumnProjectTwo() {
   return (
 <article className="article-column-project-two">
-<ContentTitleProject />
-<InputEditTitleProject />
-<ContentDescriptionProject />
+  <ContentTitleProject />
+  <InputEditTitleProject />
+  <ContentDescriptionProject />
+  <InputDescriptionProject />
 </article>
   )
 }
