379620aa68ac436c5fe5a43a541b8b4d5a15a897
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-28 12:29:10 -0500

    Atom button do test for projects created

---
+++ src/components/atoms/create-project/BtnCreateTestProject.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BtnCreateTestProject() {
+  return (
+    <>
+      <button className="btn-create-test-project">Hacer test para proyectos</button>
+    </>
+  )
+}
+
+export default BtnCreateTestProject
--- src/components/molecules/create-project/ContentInfoProjectTest.tsx
+++ src/components/molecules/create-project/ContentInfoProjectTest.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BtnCreateTestProject from '../../atoms/create-project/BtnCreateTestProject'
 
 
 function ContentInfoProjectTest() {
@@ -6,6 +7,7 @@ function ContentInfoProjectTest() {
     <>
         <div className="content-info-test-project">
         <p className="info-test-project">Antes de buscar Match es obligatorio hacer el test para proyectos, dar click en el siquiente botón para que lo redirija a la página para hacer el test:</p>
+        <BtnCreateTestProject />
         </div>
     </>
   )
