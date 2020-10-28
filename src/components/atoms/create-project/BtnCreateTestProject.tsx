b6724e829febc8201f238cd54a9a7b2d4295aeac
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-28 13:31:24 -0500

    Route create test for projects added

--- src/components/atoms/create-project/BtnCreateTestProject.tsx
+++ src/components/atoms/create-project/BtnCreateTestProject.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function BtnCreateTestProject() {
   return (
     <>
+    <Link href="/test-proyecto">
       <button className="btn-create-test-project">Hacer test para proyectos</button>
+      </Link>
     </>
   )
 }
