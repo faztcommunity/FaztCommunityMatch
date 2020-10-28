589fca9412b04f63282c563a61e89dee083c7ea0
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-10-27 22:29:05 -0500

    Molecule input edit title project created

---
+++ src/components/molecules/create-project/InputEditTitleProject.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function InputEditTitleProject() {
+  return (
+    <>
+        <div className="content-input-title-project">
+<input className="input-title-project" type="text" placeholder="Click para poner el titulo"/>
+        </div>
+    </>
+  )
+}
+
+export default InputEditTitleProject
--- src/components/organisms/create-project/ColumnProjectTwo.tsx
+++ src/components/organisms/create-project/ColumnProjectTwo.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ContentTitleProject from '../../molecules/create-project/ContentTitleProject'
+import InputEditTitleProject from '../../molecules/create-project/InputEditTitleProject'
 
 function ColumnProjectTwo() {
   return (
 <article className="article-column-project-two">
 <ContentTitleProject />
+<InputEditTitleProject />
 </article>
   )
 }
