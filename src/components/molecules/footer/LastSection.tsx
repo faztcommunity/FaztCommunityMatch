d36ebe8d24eb7e2ac820ecbcdbedd19b8144be63
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-11-02 04:22:35 -0500

    Footer molecule created

---
+++ src/components/molecules/footer/LastSection.tsx
@@ -0,0 +1,15 @@
+import IconFazt from '../../../assets/png/icono-fazt.png'
+import IconCommunity from '../../../assets/png/icono-community.png'
+
+function LastSection() {
+  return (
+    <div className="content-gratitude-section">
+<p className="txt-gratitude">Community Match  2020 - con el apoyo de:</p>
+
+<a className="icon-fazt" href="https://www.faztweb.com/" target="_blank"><img src={IconFazt} alt=""/></a>
+<a href="https://www.linkedin.com/groups/12455671/" target="_blank"><img src={IconCommunity} alt=""/></a>
+    </div>
+  )
+}
+
+export default LastSection
--- src/components/organisms/footer/Footer.tsx
+++ src/components/organisms/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ContactSection from "../../molecules/footer/ContactSection"
 import JoinSection from "../../molecules/footer/JoinSection"
+import LastSection from "../../molecules/footer/LastSection"
 import PrincipalSection from "../../molecules/footer/PrincipalSection"
 import SupportSection from "../../molecules/footer/SupportSection"
 
@@ -14,7 +15,9 @@ function Footer() {
         <ContactSection />
         <SupportSection />
         </section>
-        <div className="content-footer-two"></div>
+        <div className="content-footer-two">
+        <LastSection />
+        </div>
     </footer>
   )
 }
